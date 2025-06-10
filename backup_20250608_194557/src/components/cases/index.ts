4b9dff62456b456764cab21050ceddc8b4c8ba77
Your Name <you@example.com>
2025-06-10 11:09:23 -0400

    Remove backup directories from Git tracking

--- backup_20250608_194557/src/components/cases/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all case management components
-export { CaseList } from './CaseList';
-export { CaseFilters, CaseStats } from './CaseFilters';
-export { CaseDetails } from './CaseDetails';
-export { NewCaseModal } from './NewCaseModal';
-
-// Re-export types
-export * from '@/types/case.types';
